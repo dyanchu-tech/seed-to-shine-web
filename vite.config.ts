09d3e014a1f2e8b5c41e4d23301da5093189fe57
dyanchu-tech <dyanchu@gmail.com>
2026-06-24 19:55:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tanstackStart(), nitro(), react()],
   base: "/"
 });
